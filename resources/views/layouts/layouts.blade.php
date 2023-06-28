15dbeaff4328a276efc8e1e9b1a528f988735ee2
albanijohanes <albanijohanes106@student.unsrat.ac.id>
2023-06-28 15:52:10 +0700

    now its laravel

---
+++ resources/views/layouts/layouts.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+
+<head>
+   <meta charset="utf-8">
+   <title>User Registration</title>
+   <link rel="stylesheet" href="regist.css">
+   <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet"
+      integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+</head>
+
+<body>
+    
+</body>
---
+++ routes/web.php
@@ -0,0 +1,28 @@
+<?php
+
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\RegisterController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+// Route::get('/', function () {
+//     return view('auth/login');
+// });
+
+Route::controller(LoginController::class)->group(function(){
+    Route::get('/', 'login')->name('login');
+});
+
+Route::controller(RegisterController::class)->group(function(){
+    Route::get('register', 'register')->name('register');
+});
