0c715e4d9816faa00e0fe461af0cba6f816aa37a
albanijohanes <101566380+albanijohanes@users.noreply.github.com>
2023-06-28 17:09:29 +0700

    Nih laravelnya nanti tutornya di discord kalo mau tanya tanya (#2)
    * nahloh kehapuskan
    
    * dah balik
    
    * now its laravel
    
    * Update README.md
    
    * Update README.md
    
    ---------
    
    Co-authored-by: albanijohanes <albanijohanes106@student.unsrat.ac.id>

---
+++ README.md
@@ -0,0 +1,13 @@
+<p align="center"><a href="https://laravel.com" target="_blank"><img src="https://raw.githubusercontent.com/laravel/art/master/logo-lockup/5%20SVG/2%20CMYK/1%20Full%20Color/laravel-logolockup-cmyk-red.svg" width="400" alt="Laravel Logo"></a></p>
+
+<h1>Ingin pull?</h1>
+<p>
+    1. buka cmd dari project ini <br>
+    2. composer install <br>
+    3. cp .env.example .env <br>
+    4. php artisan key:generate <br>
+    5. project sudah siap <br>
+</p>
+
+
+The Laravel framework is open-sourced software licensed under the [MIT license](https://opensource.org/licenses/MIT).
---
+++ resources/views/auth/register.blade.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+
+<head>
+   <meta charset="utf-8">
+   <title>User Registration</title>
+   <link rel="stylesheet" href="{{ asset('assets/css/regist.css') }}">
+   <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet"
+      integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+</head>
+
+<body>
+   <div class="wrapper d-flex">
+      <div class="title col-md-6 d-flex justify-content-center">
+         <img src="{{ asset('assets/img/logo.png') }}" alt="logo" style="width:350px; height:350px;">
+      </div>
+      <div class="col-md-6 ">
+         <form id="form_login" role="form" method="post" action="config_regis.php">
+            <div class="d-flex">
+               <div class="aa">
+                  <div class="field">
+                     <input type="text" name="nama_cus" id="nama_customer" required>
+                     <label>Nama Lengkap</label>
+                    </div>
+                    <div class="field" placeholder="Sebagai">
+                        <select id="role" name="role">
+                        <option value="Customer">Customer</option>
+                        <option value="Porter">Porter</option>
+                        </select>
+                     </div>
+                  <div class="field" placeholder="Jenis Kelamin">
+                     <select id="jk" name="jk">
+                        <option value="Pria">Pria</option>
+                        <option value="Wanita">Wanita</option>
+                     </select>
+                  </div>
+                  <div class="field">
+                     <input type="text" name="alamat" id="alamat" required>
+                     <label>Alamat</label>
+                  </div>
+               </div>
+               <div class="bb">
+                  <div class="field">
+                     <input type="number" name="no_hp" id="no_hp" pattern="08\d{10}" required>
+                     <label>Nomor Handphone</label>
+                  </div>
+                  <div class="field">
+                     <input type="text" name="usrname" id="register_username" required>
+                     <label>Username</label>
+                  </div>
+                  <div class="field">
+                     <input type="password" name="pwd" id="password" required>
+                     <label>Password</label>
+                  </div>
+               </div>
+            </div>
+            <div class="field">
+               <input type="submit">
+            </div>
+            <div class="signup-link">
+               Sudah punya akun? <a href="{{ route('login') }}">Masuk</a>
+            </div>
+         </form>
+      </div>
+   </div>
+</body>
