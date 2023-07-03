b02d7de995a06b642352627308414447f541063e
albanijohanes <101566380+albanijohanes@users.noreply.github.com>
2023-07-03 14:47:59 +0700

    fix modal (#4)
    * nahloh kehapuskan
    
    * dah balik
    
    * now its laravel
    
    * Update README.md
    
    * Update README.md
    
    * app, db, resource, routes
    
    * fix modal
    
    ---------
    
    Co-authored-by: albanijohanes <albanijohanes106@student.unsrat.ac.id>

--- routes/web.php
+++ routes/web.php
@@ -24,15 +24,12 @@ Route::group([], function () {
     Route::get('', [LoginController::class, 'login'])->name('login');
     Route::post('login', [LoginController::class, 'loginPost'])->name('login.post');
 });
-
 Route::group([], function () {
     Route::get('register', [RegisterController::class, 'register'])->name('register');
     Route::post('register', [RegisterController::class, 'registerPost'])->name('register.post');
 });
-
 Route::middleware('auth')->group(function(){
     Route::get('index', [PageController::class, 'index'])->name('index');
     Route::get('services', [PageController::class, 'services'])->name('services');
     Route::get('logout', [LoginController::class, 'logout'])->name('logout');
 });
-
