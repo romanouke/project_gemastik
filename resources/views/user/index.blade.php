75adbb34b6aba4716ad9c4ea2a98f1f1930776cb
Roma Nouke Frets Mantiri <romamantiri17@gmail.com>
2023-07-12 15:51:36 +0800

    modif dikit

--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -40,7 +40,7 @@
                     <li><a class="nav-link scrollto active" href="#hero">Beranda</a></li>
                     <li><a class="nav-link scrollto" href="#services">Layanan</a></li>
                     <li><a class="nav-link scrollto" href="#histori">Riwayat</a></li>
-                    <li><a class="nav-link scrollto" href="#about">Tentang</a></li>
+                    <li><a class="nav-link scrollto" href="#about">Promosi</a></li>
                     <li><a class="nav-link scrollto" href="{{ route('logout') }}">KELUAR</a></li>
                 </ul>
                 <i class="bi bi-list mobile-nav-toggle"></i>
@@ -129,22 +129,15 @@
                         <img src="assets/img/about.jpg" class="img-fluid" alt="">
                     </div>
                     <div class="col-lg-6 pt-4 pt-lg-0 order-2 order-lg-1 content" data-aos="fade-right">
-                        <h3>Voluptatem dignissimos provident quasi corporis</h3>
+                        <h3>Dagangan</h3>
                         <p class="fst-italic">
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
-                            labore et dolore
-                            magna aliqua.
+                            Cabai Asli Manado
                         </p>
                         <ul>
-                            <li><i class="bi bi-check-circle"></i> Ullamco laboris nisi ut aliquip ex ea commodo
-                                consequat.</li>
-                            <li><i class="bi bi-check-circle"></i> Duis aute irure dolor in reprehenderit in voluptate
-                                velit.</li>
-                            <li><i class="bi bi-check-circle"></i> Ullamco laboris nisi ut aliquip ex ea commodo
-                                consequat. Duis aute irure dolor in reprehenderit in voluptate trideta storacalaperda
-                                mastiro dolore eu fugiat nulla pariatur.</li>
+                            <li><i class="bi bi-check-circle"></i>Bersih</li>
+                            <li><i class="bi bi-check-circle"></i>Murah</li>
                         </ul>
-                        <a href="#" class="read-more">Read More <i class="bi bi-long-arrow-right"></i></a>
+                        <a href="#" class="read-more">Baca Selengkapnya <i class="bi bi-long-arrow-right"></i></a>
                     </div>
                 </div>
 
