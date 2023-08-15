a4744619beb56a3dfa9f25b34ebf453532e12f44
albanijohanes <albanijohanes106@student.unsrat.ac.id>
2023-08-15 16:37:55 +0800

    Change to PHP 7

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -2,7 +2,6 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\User;
 use Illuminate\Http\Request;
 
 class PageController extends Controller
@@ -10,7 +9,6 @@ class PageController extends Controller
     public function index(){
         return view('user/index');
     }
-
     public function services(){
         return view('user/services');
     }
